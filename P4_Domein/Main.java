aee31b1ba58513726a9f2414bfa8187960c20ec0
abd-cmd <75215408+abd-cmd@users.noreply.github.com>
2021-09-24 21:43:54 +0200

    Die zijn de Nieuwe code van P4

---
+++ P4_Domein/Main.java
@@ -0,0 +1,236 @@
+package P4_Domein;
+
+
+import P4_DAO_EN_DAOSQL.*;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static Connection connection;
+
+    public static void main(String[] args) throws SQLException {
+        connection = getConnection();
+
+        ReizigerDAOsql rdao = new ReizigerDAOsql(connection);
+        AdresDAOsql adoa = new AdresDAOsql(connection);
+        Ov_chipkaartDAOsql Odao = new Ov_chipkaartDAOsql(connection);
+
+        adoa.setRdao(rdao);
+        Odao.setRdao(rdao);
+        rdao.setAdao(adoa);
+        rdao.setOdao(Odao);
+
+        testReizigerDAO(rdao);
+        testAdresDAO(adoa);
+        testOv_chipkaartDAO(Odao);
+
+        Main.closeConnection();
+
+    }
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null){
+            String Password = "Pass.123%25";
+            String url = "jdbc:postgresql://localhost/ovchip?user=postgres&password=" + Password + "";
+            String user = "postgres";
+            Connection Con = DriverManager.getConnection(url, user, Password);
+            return Con;
+        }
+        String Password = "Pass.123%25";
+        String url = "jdbc:postgresql://localhost/ovchip?user=postgres&password=" + Password + "";
+        String user = "postgres";
+        try {
+
+            Connection Con = DriverManager.getConnection(url, user, Password);
+            return Con;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+    public static Connection closeConnection(){
+        try {
+
+            connection.close();
+            return connection;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    private static void testReizigerDAO(ReizgerDAO rdao) throws SQLException {
+
+
+        System.out.println("\n---------- Test ReizigerDAO -------------");
+
+        // Haal alle reizigers op uit de database
+        //findAll
+        List<Reiziger> reizigers = rdao.findAll();
+        System.out.println();
+        System.out.println();
+
+
+        System.out.println("[Test] ReizigerDAO.findAll() geeft de volgende reizigers:");
+        for (Reiziger r : reizigers) {
+            System.out.println(r);
+        }
+        System.out.println();
+
+
+
+
+
+        // Maak een nieuwe reiziger aan en persisteer deze in de database
+        // save
+        String gbdatum = "1981-03-14";
+        Date date = Date.valueOf(gbdatum);
+        Reiziger sietske = new Reiziger(77, "S", "", "Boers", date);
+        System.out.print("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.save() ");
+        rdao.save(sietske);
+        System.out.println("het werkt");
+
+        System.out.println();
+
+        reizigers = rdao.findAll();
+        System.out.println(reizigers.size() + " reizigers\n");
+
+        // Voeg aanvullende tests van de ontbrekende CRUD-operaties in.
+        System.out.println();
+        //Update
+        String gboortdatum = "1998-09-17";
+        Date date2 = Date.valueOf(gboortdatum);
+        Reiziger r = new Reiziger(1,"A","Almoneim", "Alshami", date2);
+        rdao.update(r);
+        System.out.println("het werkt");
+        System.out.println();
+
+        //Delete
+        rdao.delete(sietske);
+
+
+        System.out.println();
+        // findById
+        System.out.println("findById");
+        System.out.println(rdao.findById(3));
+        System.out.println("het werkt");
+
+        System.out.println();
+        //findByGbdatum
+        System.out.println("findByGbdatum");
+        String gboortdatum2 = "1998-08-11";
+        Date date3 = Date.valueOf(gboortdatum2);
+        System.out.println(rdao.findByGbdatum(date3));
+        System.out.println("het werkt");
+
+
+        String gboortdatum1 = "2000-01-01";
+        Date date1 = Date.valueOf(gboortdatum1);
+        Reiziger r5 = new Reiziger(800,"jouhar","null", "Alshami", date1);
+        Adres adres1 = new Adres(5,"3521BV","373 BISA","croeslaan","Utrecht",r5);
+        rdao.save(r5);
+
+    }
+
+
+    private static void testAdresDAO(AdresDAO adao)throws SQLException{
+
+        System.out.println("\n---------- Test AdresDAO -------------");
+
+        // Haal alle Adresen op uit de database
+        //findAll
+        List<Adres> adressen = adao.findAll();
+        System.out.println("[Test] AdresDAO.findAll() geeft de volgende Adressen:");
+        for (Adres adres : adressen) {
+            System.out.println(adres );
+
+        }
+        String gboortdatum = "1998-09-17";
+        Date date1 = Date.valueOf(gboortdatum);
+        Reiziger r = new Reiziger(1,"A","Almoneim", "Alshami",date1);
+
+
+        // Maak een nieuwe Adres aan en persisteer deze in de database
+
+        System.out.println();
+        //Save
+        String gboortdatum2 = "1998-09-17";
+        Date date2 = Date.valueOf(gboortdatum2);
+        Reiziger r2 = new Reiziger(1,"A","Almoneim", "Alshami",date2);
+        Adres adres = new Adres(6,"3555VM","71","maasdijkstraat","Utrecht",r);
+        System.out.print("[Test] Eerst " + adressen.size() + " adressen, na AdresDAO.save() ");
+        adao.save(adres);
+
+        System.out.println();
+        adressen = adao.findAll();
+        System.out.println(adressen.size() + " Adressen\n");
+
+        System.out.println();
+        //findByReiziger
+
+        System.out.println(adao.findByReiziger(r));
+
+
+        System.out.println();
+        //Delete
+        adao.delete(adres);
+
+        System.out.println();
+        //Update
+        Adres adres1 = new Adres(5,"3521BV","373 BISA","croeslaan","Utrecht",r2);
+        adao.update(adres1);
+
+    }
+
+
+    private static void testOv_chipkaartDAO(Ov_chipkaartDAO odao)throws SQLException{
+
+        ReizigerDAOsql rdao = new ReizigerDAOsql(connection);
+
+        System.out.println("\n---------- Test Ov_chipkaartDAO -------------");
+
+
+        // Maak een nieuwe ov_chipkaart aan en persisteer deze in de database
+        String gboortdatum1 = "1998-09-17";
+        Date date1 = Date.valueOf(gboortdatum1);
+        Reiziger r5 = new Reiziger(150,"A","Almoneim", "Alshami", date1);
+        String gboortdatum2 = "2020-01-01";
+        Ov_chipkaart Ov_chipkaart = new Ov_chipkaart(44444,java.sql.Date.valueOf(gboortdatum2),3,25,r5);
+        rdao.save(r5);
+
+//
+        // Haal alle ov_chipkaarts op uit de database
+        //findAll
+        List<Ov_chipkaart> ov_chipkaarts = odao.findAll();
+        System.out.println("[Test] Ov_chipkaartDAO.findAll() geeft de volgende Ov_chipkaarten:");
+        for (Ov_chipkaart ov_chipkaart : ov_chipkaarts) {
+            System.out.println(ov_chipkaart );
+
+        }
+
+
+        //update
+        Ov_chipkaart Ov_chipkaart2 = new Ov_chipkaart(44444,java.sql.Date.valueOf(gboortdatum2),4,50,r5);
+        odao.update(Ov_chipkaart2);
+
+        //findbyReiziger
+        System.out.println(odao.findByReiziger(r5));
+        System.out.println("iets");
+
+        //delete
+        odao.delete(Ov_chipkaart2);
+
+
+    }
+
+
+
+
+}
---
+++ P4_Domein/Reiziger.java
@@ -0,0 +1,131 @@
+package P4_Domein;
+
+import java.util.ArrayList;
+import java.sql.Date;
+import java.util.List;
+
+public class Reiziger {
+
+        private  int reizigerId;
+        private  String voorletters;
+        private  String tussenvoegsel;
+        private  String achternaam;
+        private  Date geboortedatum;
+        private Adres adres;
+        private List<Ov_chipkaart> ov_chipkaarts;
+
+        public Reiziger(int id , String voorletters , String tussenvoegsel, String achternaam, Date geboortedatum){
+            reizigerId = id;
+            this.voorletters = voorletters;
+            this.tussenvoegsel = tussenvoegsel;
+            this.achternaam = achternaam;
+            this.geboortedatum = geboortedatum;
+            this.ov_chipkaarts = new ArrayList<>();
+        }
+
+
+
+    public List<Ov_chipkaart> getOv_chipkaarts() {
+        return ov_chipkaarts;
+    }
+
+    public void setOv_chipkaarts(List<Ov_chipkaart> ov_chipkaarts) {
+        this.ov_chipkaarts = ov_chipkaarts;
+    }
+
+
+
+    public Adres getAdres() {
+        return adres;
+    }
+
+    public void setAdres(Adres adres) {
+        this.adres = adres;
+    }
+
+    public  int getReizigerId() {
+            return reizigerId;
+        }
+
+        public    Date getGeboortedatum() {
+            return geboortedatum;
+        }
+
+        public void setGeboortedatum(Date geboortedatum) {
+            this.geboortedatum = geboortedatum;
+        }
+
+        public    String getAchternaam() {
+            return achternaam;
+        }
+
+        public void setAchternaam(String achternaam) {
+            this.achternaam = achternaam;
+        }
+
+        public  String getTussenvoegsel() {
+            return tussenvoegsel;
+        }
+
+        public  String getVoorletters() {
+            return voorletters;
+        }
+
+        public void setVoorletters(String voorletters) {
+            this.voorletters = voorletters;
+        }
+
+        public void setTussenvoegsel(String tussenvoegsel) {
+            this.tussenvoegsel = tussenvoegsel;
+        }
+
+        public void setReizigerId(int reizigerId) {
+            this.reizigerId = reizigerId;
+        }
+
+        public void voegOv_chipkaartToe(Ov_chipkaart ov_chipkaart){
+            ov_chipkaarts.add(ov_chipkaart);
+        }
+
+        public void remove_Ov_chipkaart(Ov_chipkaart ov_chipkaart){
+            ov_chipkaarts.remove(ov_chipkaart);
+        }
+
+
+        @Override
+        public String toString() {
+            //reiziger gegevens
+            String a = "";
+            String ov = "";
+            if(adres!=null && ov_chipkaarts.size() != 0){
+                ov = ov_chipkaarts.toString();
+                a = adres.toString();
+            }
+           return  voorletters + tussenvoegsel +" " +  achternaam + "  " +  geboortedatum +" " + reizigerId +" "  + " " + a + " " + ov;
+        }
+            
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
